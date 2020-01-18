02f05b3f6d3dd83bff70c2810056b57552878c76
Boris Demeshev <boris.demeshev@gmail.com>
2020-01-18 14:54:16 +0300

    03 and 10 file naming

--- setup/setup_boris.R
+++ setup/setup_boris.R
@@ -8,3 +8,31 @@ child = tibble::tibble(r = rep(0, n_chap),
 child$r[1] = 1
 child$py[1] = 1
 child$stata[1] = 1
+
+child$r[2] = 1
+child$py[2] = 0
+child$stata[2] = 0
+
+child$r[3] = 1
+child$py[3] = 0
+child$stata[3] = 0
+
+child$r[4] = 1
+child$py[4] = 0
+child$stata[4] = 0
+
+child$r[5] = 1
+child$py[5] = 0
+child$stata[5] = 0
+
+child$r[8] = 1
+child$py[8] = 0
+child$stata[8] = 0
+
+
+child$r[10] = 1
+child$py[10] = 0
+child$stata[10] = 0
+
+
+
